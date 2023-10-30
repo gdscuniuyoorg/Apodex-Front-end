063279909b31aa0b1cdce2e143c5da7bfc740f2a
kufre-abasi <kufreabasibassey3@gmail.com>
2023-10-30 21:10:37 +0100

    docs: counter button deleted

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,10 @@
-import CounterButtons from '../components/CounterButtons'
 
 export default function Home() {
   return (
     <div>
-      <CounterButtons />
+      <div className="flex flex-col text-center">
+        <h1>landing page here</h1>
+      </div>
     </div>
-  )
+  );
 }
